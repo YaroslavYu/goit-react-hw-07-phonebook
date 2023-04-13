fa11f1c7ae51447d72cf9ca4786aceeae4537cf5
YaroslavYu <yaroslavyu801@gmail.com>
2023-04-13 16:59:05 +0300

    fix the bugs

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -19,7 +19,8 @@ export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (contact, thunkAPI) => {
     try {
-      await axios.post('/contacts', contact);
+      const response = await axios.post('/contacts', contact);
+      return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
     }
